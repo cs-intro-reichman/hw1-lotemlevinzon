e48a6220d33fc0fa5fdc279df843967ea1235dca
Lotem Levinzon <lotem.levinzon@post.runi.ac.il>
2025-11-05 17:35:25 +0200

    Completed assignment

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,13 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int lim = Integer.parseInt(args[0]);
+		int a = (int)((Math.random() * lim) + 1);
+		int b = (int)((Math.random() * lim) + 1);
+		int c = (int)((Math.random() * lim) + 1);
+		int minNum = Math.min(Math.min(a,b), c);
+		int maxNum = Math.max(Math.max(a,b), c);
+		int midNum = a + b + c - minNum - maxNum;
+		System.out.println(minNum + " " + midNum + " " + maxNum);
 	}
 }
--- TimeFormat.java
+++ TimeFormat.java
@@ -12,6 +12,23 @@ public class TimeFormat {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // Replace this comment with the rest of your code
+		if (hours > 12  && hours < 24) {
+			System.out.print(hours - 12);
+		} else if (hours <= 12 && hours > 0) {
+            System.out.print(hours - 12);
+		} else if (hours == 0) {
+			System.out.print(0);
+		}
+		System.out.print(":");
+  if (minutes < 10) {
+			System.out.print("0" + minutes);
+		} else {
+            System.out.print(minutes);
+		} 
+	if (hours < 12){
+		System.out.println(" AM");
+	} else {
+		System.out.println(" PM");
+	}
 	}
 }
